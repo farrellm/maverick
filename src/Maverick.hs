58bb57aa09b92a018b32132dd72fe58b8c5224c8
Matthew Farrell <farrellm@alum.mit.edu>
2024-10-08 08:55:57 -0400

    use mutable deck

--- src/Maverick.hs
+++ src/Maverick.hs
@@ -5,10 +5,12 @@ module Maverick (printEquityTable) where
 
 import Data.Text qualified as T
 import Data.Vector qualified as V
+import Data.Vector.Mutable (IOVector)
+import Data.Vector.Mutable qualified as VM
 import Maverick.Score (score)
 import Maverick.Types
 import Numeric (showFFloat)
-import System.Random.MWC (GenIO, createSystemRandom)
+import System.Random.MWC (GenIO, UniformRange (uniformRM), createSystemRandom)
 import System.Random.MWC.Distributions (uniformShuffle)
 
 hands :: V.Vector (V.Vector Card)
@@ -35,11 +37,22 @@ deck = V.fromList $ do
 
 type EquityIO = StateT (V.Vector Card) (ReaderT GenIO IO)
 
+type EquityIO' = StateT Int (ReaderT (GenIO, IOVector Card) IO)
+
 deal :: Int -> EquityIO (V.Vector Card)
 deal n = do
   h <- V.unsafeTake n <$> get
   modify' (V.unsafeDrop n)
   pure h
+
+draw :: EquityIO' Card
+draw = do
+  (gen, d) <- ask
+  i <- get
+  modify' (+ 1)
+  j <- uniformRM (i, VM.length d - 1) gen
+  VM.unsafeSwap d i j
+  VM.unsafeRead d i
 
 shuffle :: EquityIO ()
 shuffle = do
@@ -71,6 +84,32 @@ calcEquity cs n = do
   (/ fromIntegral m) . getSum
     <$> timesM m (Sum <$> evaluatingStateT deck' (playHand p n))
 
+playHand' :: Hand -> Int -> EquityIO' Double
+playHand' p n = do
+  community <- (hand <$> draw) <+^ draw <+^ draw <+^ draw <+^ draw
+  ss <- V.replicateM n $ do
+    score <$> (pure community <+^ draw <+^ draw)
+  let s = score $ community <> p
+      best = max s (V.maximum ss)
+  if s == best
+    then pure (1 / (1 + countEqual best ss))
+    else pure 0
+  where
+    countEqual x = getSum . V.foldMap' (\y -> if x == y then 1 else 0)
+    (<+^) = liftA2 (<+)
+
+calcEquity' :: V.Vector Card -> Int -> ReaderT GenIO IO Double
+calcEquity' cs n = do
+  gen <- ask
+  deck' <- V.unsafeThaw $ V.filter (`notElem` cs) deck
+  let p = V.foldMap' hand cs
+      m = 10000
+  liftIO $
+    (/ fromIntegral m) . getSum
+      <$> timesM
+        m
+        (Sum <$> (usingReaderT (gen, deck') . evaluatingStateT 0 $ playHand' p n))
+
 timesM :: (Monoid m, Monad f) => Int -> f m -> f m
 timesM cnt0 x = loop cnt0 mempty
   where
@@ -84,7 +123,7 @@ printEquityTable = do
   gen <- createSystemRandom
   usingReaderT gen $ do
     for_ hands $ \pocket -> do
-      es <- traverse (calcEquity pocket) [1 .. 9]
+      es <- traverse (calcEquity' pocket) [1 .. 9]
       putText (T.intercalate " " (V.toList $ render <$> pocket))
       putText " | "
       putTextLn (T.intercalate " " (toText . (flip (showFFloat (Just 2)) "") <$> es))
